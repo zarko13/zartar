83fbaa084df6d2f5f313ec720d29c8805b799c5e
zarko13 <zarsarenac@gmail.com>
2024-11-22 15:18:52 +0100

    all set

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicController::class, 'index']);
-Route::get('async/execute-command', [PublicController::class, 'asyncExecuteCommand']);
+Route::post('async/execute-command', [PublicController::class, 'asyncExecuteCommand']);

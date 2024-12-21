67b064fd591c731d36e1afafab6d781a5cbd712c
zarko13 <zarsarenac@gmail.com>
2024-12-21 15:05:30 +0100

    Git commands

--- app/Repositories/Executor/GitService.php
+++ app/Repositories/Executor/GitService.php
@@ -9,49 +9,6 @@ use Illuminate\Support\Facades\Process;
 
 class GitService
 {
-    public static function branch(){
-
-        $errors = null;
-        $data = [];
-
-        try {
-
-            $branch = Process::run('git branch')->output();
-
-
-
-            $data['message'] = 'Your current branch is:' . $branch;
-        
-        } catch (Exception $error){
-            $errors[] = 'Failed to detect branch';
-            Log::error('Failed to detect branch.Error:'.$error);
-            
-        }
-
-        return new ServiceResponse($errors, $data);
-
-    }
-
-    public static function push(){
-
-        $errors = null;
-        $data = [];
-
-        try {
-
-            $result = Process::run('git push');
-            $data['message'] = $result->successful() ? $result->errorOutput() : $result->output();
-        
-        } catch (Exception $error){
-            $errors[] = 'Failed to detect branch';
-            Log::error('Failed to detect branch.Error:'.$error);
-            
-        }
-
-        return new ServiceResponse($errors, $data);
-
-    }
-
     public static function add(){
 
         $errors = null;
@@ -93,5 +50,148 @@ class GitService
 
     }
 
+    public static function push(){
+
+        $errors = null;
+        $data = [];
+
+        try {
+
+            $result = Process::run('git push');
+            $data['message'] = $result->successful() ? $result->errorOutput() : $result->output();
+        
+        } catch (Exception $error){
+            $errors[] = 'Failed to detect branch';
+            Log::error('Failed to detect branch.Error:'.$error);
+            
+        }
+
+        return new ServiceResponse($errors, $data);
+
+    }
+
+    public static function branch(){
+
+        $errors = null;
+        $data = [];
+
+        try {
+
+            $branch = Process::run('git branch')->output();
+
+
+
+            $data['message'] = 'Your current branch is:' . $branch;
+        
+        } catch (Exception $error){
+            $errors[] = 'Failed to detect branch';
+            Log::error('Failed to detect branch.Error:'.$error);
+            
+        }
+
+        return new ServiceResponse($errors, $data);
+
+    }
+
+    public static function checkout($branch){
+
+        $errors = null;
+        $data = [];
+
+        try {
+
+            $result = Process::run('git checkout ' . $branch);
+            $data['message'] = $result->successful() ? $result->errorOutput() : $result->output();
+        
+        } catch (Exception $error){
+            $errors[] = 'Failed to detect branch';
+            Log::error('Failed to detect branch.Error:'.$error);
+            
+        }
+
+        return new ServiceResponse($errors, $data);
+
+    }
+    
+    public static function reset(){
+
+        $errors = null;
+        $data = [];
+
+        try {
+
+            $result = Process::run('git reset --hard');
+            $data['message'] = $result->successful() ? $result->errorOutput() : $result->output();
+        
+        } catch (Exception $error){
+            $errors[] = 'Failed to detect branch';
+            Log::error('Failed to detect branch.Error:'.$error);
+            
+        }
+
+        return new ServiceResponse($errors, $data);
+
+    }
+
+    public static function stash(){
+
+        $errors = null;
+        $data = [];
+
+        try {
+
+            $result = Process::run('git stash');
+            $data['message'] = $result->successful() ? $result->errorOutput() : $result->output();
+        
+        } catch (Exception $error){
+            $errors[] = 'Failed to detect branch';
+            Log::error('Failed to detect branch.Error:'.$error);
+            
+        }
+
+        return new ServiceResponse($errors, $data);
+
+    }
+
+    public static function apply(){
+
+        $errors = null;
+        $data = [];
+
+        try {
+
+            $result = Process::run('git stash apply');
+            $data['message'] = $result->successful() ? $result->errorOutput() : $result->output();
+        
+        } catch (Exception $error){
+            $errors[] = 'Failed to detect branch';
+            Log::error('Failed to detect branch.Error:'.$error);
+            
+        }
+
+        return new ServiceResponse($errors, $data);
+
+    }
+
+
+    public static function pull(){
+
+        $errors = null;
+        $data = [];
+
+        try {
+
+            $result = Process::run('git pull');
+            $data['message'] = $result->successful() ? $result->errorOutput() : $result->output();
+        
+        } catch (Exception $error){
+            $errors[] = 'Failed to detect branch';
+            Log::error('Failed to detect branch.Error:'.$error);
+            
+        }
+
+        return new ServiceResponse($errors, $data);
+
+    }
    
 }
